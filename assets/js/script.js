7a53a6cf12007b4680ab363ff35939c092824298
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-27 11:43:47 +0100

    js with pseudocode

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+/*
+Milestone 3
+Inseriamo un foglio JavaScript ed effettuiamo una chiamata AJAX all’API,
+sfruttando la risposta per generare dinamicamente in pagina una serie di foto!
+*/
+
+//assegno la mia API key ad una variabile
+//utilizzo quella variabile per feettuare una chiamata ajax
+//il dato restituito dalla chiamata lo assegno a sua volta ad una variabile
+//eseguo un ciclo forEach o for semplice per iterare nel dato restituito dalla API key
+//sapendo che il dato restituito dalla chiamata è un array di oggetti,
+//assegno il valore delle proprietà degli oggetti agli elementi html
+//incrocio le dita e se non funziona mi chiudo in camera a piangere :'C
