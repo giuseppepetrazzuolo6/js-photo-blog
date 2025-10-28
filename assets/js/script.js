2b768136ca88991c616ee9fab86c16e858d70485
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2025-10-28 10:31:58 +0100

    pseudocode

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,3 +52,22 @@ axios.get(apiUrl)
     .catch(error => {
         console.error('Errore nella chiamata API:', error);
     });
+/*
+Milestone 1
+Facciamo in modo di creare un overlay che copra l’intera pagina e all’interno, centrata,
+disponiamo un’immagine qualunque ed un button di chiusura.
+
+Milestone 2
+Facciamo sparire l’overlay con l’aiuto di una classe CSS che imposti il display: none .
+Dopodiché facciamo sì che cliccando una qualunque foto. L’overlay ricompaia.
+Cliccando invece il button di chiusura, l’overlay scompare nuovamente.
+
+Milestone 3
+Inseriamo il pezzo di logica finale: quando una foto viene cliccata,
+dobbiamo fare in modo che sia proprio quella foto a essere mostrata all’interno dell’overlay.
+*/
+
+//richiamo gli elementi HTML dell'overlay
+//aggiungo un evento che rimuove la classe d-none all'overlay cliccando sulle immagini
+//assegno al tag img dell'overlay il valore dell'immagine cliccata (src)
+//aggiungo un evento al bottone che aggiunge nuovamente la classe d-none all'overlay
